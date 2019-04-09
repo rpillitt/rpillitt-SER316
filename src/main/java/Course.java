3c3c3ac076b94de1abea7895575a32f6f3089167
rpillitt <rpillitt@asu.edu>
2019-04-08 20:24:53 -0700

    StaticAnalysis tasks complete

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -13,108 +13,122 @@ import java.util.HashMap;
 
 
 public class Course {
-    
-    public HashMap<String, Integer> points = new HashMap<>(); // maps student names (asurite) to their points
-    private String Name; // course name
+
+    // maps student names (asurite) to their points
+    public HashMap<String, Integer> points = new HashMap<>(); 
+    private String name; // course name
+    ArrayList<Student> students  = new ArrayList<Student>();
 
 
-    public Course(String name){
-        this.SetName(name);
+    public Course(String name) {
+        this.setName(name);
     }
 
-    public String GetName() {
-        return Name;
+    public String getName() {
+        return name;
     }
 
-    public void SetName(String name) {
-        this.Name = name;
+    public void setName(String name) {
+        this.name = name;
     }
-    
 
+
+    /**Prints the values of students and their average within the course.
+     * 
+     */
     public void printCourseStats() {
-       ArrayList<Integer> values = new ArrayList<Integer>(points.values());
+        //SER316 TASK 2 SPOTBUGS FIX
+        //ArrayList<Integer> values = new ArrayList<Integer>(points.values());
 
         System.out.print("Average Grades without max and without min: ");
         System.out.println(this.calculateAverageWithoutMinWithoutMax());
     }
-    
-    
-    // REACH at least 95% Code coverage (assign 3)
-    // method to draw node graph for
-    // should throw null pointer for empty points member
-    // negative points should be ignored
-    // max value and min value should be removed - (if doubles then only the first occurrence)
-    // if just one or two values no values will be omitted
+
+    /**Calculates averages with no min or max values provided
+     * 
+     * @return
+     * @throws NullPointerException thrown when no values in points.
+     */
     public double calculateAverageWithoutMinWithoutMax() throws NullPointerException {
         ArrayList<Integer> collection = new ArrayList<Integer>(points.values());
-        
-         int counter = 0;
-         int min = Integer.MAX_VALUE;
-         int max = Integer.MIN_VALUE;
-         
-         //SER316 Start
-         for (int i = collection.size(); i > 0; i--) {
-        	 if (collection.get(i-1) < 0) {
-        		 collection.remove(i-1);
-        	 }
-         }
-         //SER316 End
-         //Above section added to truly throw out negative points and thus not count them
-         
-         if (collection.size() == 1) {
+
+        int counter = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        //SER316 Start
+        for (int i = collection.size(); i > 0; i--) {
+            if (collection.get(i - 1) < 0) {
+                collection.remove(i - 1);
+            }
+        }
+        //SER316 End
+        //Above section added to truly throw out negative points and thus not count them
+
+        if (collection.size() == 1) {
             return collection.get(0);
-         }
-         else if (collection.size() == 2) {
-            return (double)(collection.get(0) + collection.get(1))/2;
-         }
-         else {
+        } else if (collection.size() == 2) {
+            return (double)(collection.get(0) + collection.get(1)) / 2;
+        } else {
             int allPoints = 0;
             for (int point: collection) {
-            	counter++;
-            	if (point < min) {
-            		min = point;
-            		}
-                if (point > max){
+                counter++;
+                if (point < min) {
+                    min = point;
+                }
+                if (point > max) {
                     max = point;
                 }
                 allPoints = allPoints + point;
             }  
-            int totalPoints = allPoints-max-min;
-                return totalPoints/(double)(counter-2);
-                //Above line had (counter-1), which does not work when removing two values
+            int totalPoints = allPoints - max - min;
+            return totalPoints / (double) (counter - 2);
+            //Above line had (counter-1), which does not work when removing two values
         }
     }
-    
+
     // REACH at least 95% Code coverage  (assign 3)
-    // if student with the name (asurite member) is not yet included student needs to be added to student list 
+    // if student with the name (asurite member) is not yet included 
+    // student needs to be added to student list 
     // sets points for a student 
+    
+    /**Sets points for a given student in points HashMap.
+     * 
+     * @param name Name of student to change points
+     * @param points Points value to be assigned to the student
+     */
     public void set_points(String name, int points) {
-    	//System.out.println(points);
-    	//SER316 Start
-    	ArrayList<String> asurites = new ArrayList<>();
-    	for (int i = 0; i < this.getStudents().size(); i++) {
-    		asurites.add(this.getStudents().get(i).getAsurite());
-    	}    		
-    	
-    	if (!asurites.contains(name)) {
-    		this.addStudent(new Student(name, null));
-    		this.points.put(name,  points);
-    	}
-    	else {
-    		this.points.put(name, points);
-    	}
-    	//SER316 End
+        //System.out.println(points);
+        //SER316 Start
+        ArrayList<String> asurites = new ArrayList<>();
+        for (int i = 0; i < this.getStudents().size(); i++) {
+            asurites.add(this.getStudents().get(i).getAsurite());
+        }
+
+        if (!asurites.contains(name)) {
+            this.addStudent(new Student(name, null));
+            this.points.put(name,  points);
+        } else {
+            this.points.put(name, points);
+        }
+        //SER316 End
     }
-    
-    
+
+
     // REACH at least 95% Code coverage  (assign 3)
-    // Students should only be added when they are not yet in the course (names (asurite member) needs to be unique)
-    ArrayList<Student> students  = new ArrayList<Student>();
+    // Students should only be added when they are not yet in the course 
+    // (names (asurite member) needs to be unique)
+    
+    /**Adds a student to the collection of students in a course.
+     * 
+     * @param s The student object to be added to the course
+     * @return Boolean value representing successful add
+     */
     public boolean addStudent(Student s) {
         //SER316 Start
-    	boolean added = false;
+        boolean added = false;
         if (!students.contains(s) && s != null) {
-        	students.add(s);
+            students.add(s);
             points.put(s.getAsurite(), -1);
             added = true;
         }
@@ -124,24 +138,20 @@ public class Course {
     }
 
 
-    public HashMap<String, Integer> getPoints(){
+    public HashMap<String, Integer> getPoints() {
         return points;
     }
-    
+
     public int getStudent_Points(Student student) {
         return points.get(student.getAsurite());
     }
 
-    
-    public ArrayList<Student> getStudents(){
+
+    public ArrayList<Student> getStudents() {
         return students;
     }
-    
-    public HashMap<String, Integer> countOccurencesLetterGrades() throws NullPointerException{
-     	return null;
-   
+
+    public HashMap<String, Integer> countOccurencesLetterGrades() throws NullPointerException {
+        return null;
     }
-  
- 
-  
- }
+}
--- src/test/java/GivenBlackbox.java
+++ src/test/java/GivenBlackbox.java
@@ -25,13 +25,13 @@ import java.lang.reflect.Constructor;
 @RunWith(Parameterized.class)
 public class GivenBlackbox {
     private Class<Course> classUnderTest;
-    
-    
+
+
     @SuppressWarnings("unchecked")
     public GivenBlackbox(Object classUnderTest) {
         this.classUnderTest = (Class<Course>) classUnderTest;
     }
-    
+
     // Defining all the classes that need to be tested
     @Parameters
     public static Collection<Object[]> courseGradesUnderTest() {
@@ -45,13 +45,13 @@ public class GivenBlackbox {
         };
         return Arrays.asList(classes);
     }
-    
+
     // method to call the correct constructor
     private Course createCourse(String name) throws Exception {
         Constructor<Course> constructor = classUnderTest.getConstructor(String.class);
         return constructor.newInstance(name);
     }
-    
+
 
     // A sample course
     Course twoStudent;
@@ -66,14 +66,14 @@ public class GivenBlackbox {
     HashMap<String, Integer> negativeSoloExpected = new HashMap<>();
     Course lonelyClass;
     HashMap<String, Integer> lonelyExpected = new HashMap<>();
-    
-    
+
+
     @Before
     public void setUp() throws Exception {
-        
+
         // all courses should be created like this
-        
-        
+
+
         // Course created with two Students having
         twoStudent = createCourse("SER316");
         twoStudent.set_points("Hanna",100);
@@ -84,48 +84,48 @@ public class GivenBlackbox {
         twoStudentExpected.put("C", 0);
         twoStudentExpected.put("D", 0);
         twoStudentExpected.put("F", 0);
-        
+
         zeroStudent = createCourse("SER334");
-        
+
         failingClass = createCourse("CSE240");
         failingClass.set_points("Rob",0);
         failingClass.set_points("Trev",0);
-        
+
         justMissedClass = createCourse("BIO181");
         justMissedClass.set_points("Brown", 100);
         justMissedClass.set_points("John", 80);
         justMissedClass.set_points("James", 65);
         justMissedClass.set_points("Jacob", 50);
         justMissedClass.set_points("Jordan", 35);
-        
+
         justMissedExpected.put("A", 1);
         justMissedExpected.put("B", 1);
         justMissedExpected.put("C", 1);
         justMissedExpected.put("D", 1);
         justMissedExpected.put("F", 1);
-        
+
         negativeClass = createCourse("SER321");
         negativeClass.set_points("Andrew", 100);
         negativeClass.set_points("Jacopo", -1);
-        
+
         negativeClassExpected.put("A", 1);
         negativeClassExpected.put("B", 0);
         negativeClassExpected.put("C", 0);
         negativeClassExpected.put("D", 0);
         negativeClassExpected.put("F", 0);
-        
+
         negativeSoloClass = createCourse("CHM111");
         negativeSoloClass.set_points("Jason", -5);
-        
+
         negativeSoloExpected.put("A", 0);
         negativeSoloExpected.put("B", 0);
         negativeSoloExpected.put("C", 0);
         negativeSoloExpected.put("D", 0);
         negativeSoloExpected.put("F", 0);
-        
+
         lonelyClass = createCourse("EGR280");
         lonelyClass.set_points("Bella", 1);
-        
+
         lonelyExpected.put("A", 1);
         lonelyExpected.put("B", 0);
         lonelyExpected.put("C", 0);
@@ -141,46 +141,46 @@ public class GivenBlackbox {
         //System.out.println(ans);
         assertTrue(ans.equals(twoStudentExpected));
     }
-    
-    @Test(expected=NullPointerException.class)
+
+    @Test(expected = NullPointerException.class)
     public void zeroStudent() {
-    	@SuppressWarnings("unused")
-		HashMap<String, Integer> ans = zeroStudent.countOccurencesLetterGrades();
-    	//System.out.println(ans);
+        //SER316 TASK 2 SPOTBUGS FIX
+        zeroStudent.countOccurencesLetterGrades();
+        //System.out.println(ans);
     }
-    
-    @Test(expected=ArithmeticException.class)
+
+    @Test(expected = ArithmeticException.class)
     public void failingClass() {
-    	@SuppressWarnings("unused")
-		HashMap<String, Integer> ans = failingClass.countOccurencesLetterGrades();
-    	//System.out.println(ans);
+        //SER316 TASK 2 SPOTBUGS FIX
+        failingClass.countOccurencesLetterGrades();
+        //System.out.println(ans);
     }
-    
+
     @Test
     public void justMissedClass() {
-    	HashMap<String, Integer> ans = justMissedClass.countOccurencesLetterGrades();
-    	//System.out.println(ans);
-    	assertTrue(ans.equals(justMissedExpected));
+        HashMap<String, Integer> ans = justMissedClass.countOccurencesLetterGrades();
+        //System.out.println(ans);
+        assertTrue(ans.equals(justMissedExpected));
     }
-    
+
     @Test
     public void negativeClass() {
-    	HashMap<String, Integer> ans = negativeClass.countOccurencesLetterGrades();
-    	//System.out.println(ans);
-    	assertTrue(ans.equals(negativeClassExpected));
+        HashMap<String, Integer> ans = negativeClass.countOccurencesLetterGrades();
+        //System.out.println(ans);
+        assertTrue(ans.equals(negativeClassExpected));
     }
-    
+
     @Test
     public void negativeSolo() {
-    	HashMap<String, Integer> ans = negativeSoloClass.countOccurencesLetterGrades();
-    	//System.out.println(ans);
-    	assertTrue(ans.equals(negativeSoloExpected));
+        HashMap<String, Integer> ans = negativeSoloClass.countOccurencesLetterGrades();
+        //System.out.println(ans);
+        assertTrue(ans.equals(negativeSoloExpected));
     }
-    
+
     @Test
     public void lonelyClass() {
-    	HashMap<String, Integer> ans = lonelyClass.countOccurencesLetterGrades();
-    	//System.out.println(ans);
-    	assertTrue(ans.equals(lonelyExpected));
+        HashMap<String, Integer> ans = lonelyClass.countOccurencesLetterGrades();
+        //System.out.println(ans);
+        assertTrue(ans.equals(lonelyExpected));
     }
 }
